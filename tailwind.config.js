c1601bcab24b31459b32b1f111fe5b94811f5203
dannyil <freedman.daniel.work@gmail.com>
2024-02-15 18:49:00 +0200

    tailwind-config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+      prefix: 'tw-',
+      content: [],
+      theme: {
+        extend: {},
+      },
+      plugins: [require("daisyui")],
+   }
 
