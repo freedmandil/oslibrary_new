aeb6047da9bdc705e0f39b6ca33940ab1a481a71
dannyil <freedman.daniel.work@gmail.com>
2024-02-16 01:03:38 +0200

    migrations table creation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,14 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usr_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('alt_name')->nullable();
+            $table->string('email2')->nullable();
+            $table->string('phone1')->nullable();
+            $table->string('phone2')->nullable();
+            $table->string('address1')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('address3')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('zip_post_code')->nullable();
             $table->rememberToken();
+            $table->integer('cat_id');
+            $table->integer('access_level')->nullable();
+            $table->dateTime('date_created')->nullable();
             $table->timestamps();
+            $table->foreign('usr_cat_id')->references('id')->on('usr_cats');
+
         });
     }
 
