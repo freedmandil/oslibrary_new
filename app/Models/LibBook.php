ba6faaf52972476b1b8182fa7b4f463dfb0fbe50
dannyil <freedman.daniel.work@gmail.com>
2024-02-24 22:44:06 +0200

    Library Book model setup

--- app/Models/LibBook.php
+++ app/Models/LibBook.php
@@ -28,10 +28,40 @@ class LibBook extends Model
         return $this->belongsTo(LibPublisher::class, 'publisher_id');
     }
 
+
+    public function tax_subcategory()
+    {
+        // Assuming Publisher model and publisher_id column in books table
+        return $this->belongsTo(TaxSubcat::class, 'subcategory_id');
+    }
+    public function tax_category()
+    {
+        // Assuming Publisher model and publisher_id column in books table
+        return $this->belongsTo(TaxCategory::class, 'book_category_id');
+    }
+
+    public function tax_topic()
+    {
+        // Assuming Publisher model and publisher_id column in books table
+        return $this->belongsTo(TaxTopic::class, 'book_topic_id');
+    }
+
+    public function tax_group()
+    {
+        // Assuming Publisher model and publisher_id column in books table
+        return $this->belongsTo(TaxGroup::class, 'group_id');
+    }
+
     public function sys_language()
     {
         // Assuming Publisher model and publisher_id column in books table
         return $this->belongsTo(SysLanguage::class, 'publisher_id');
+    }
+
+    public function loc_assignment()
+    {
+        // Assuming Publisher model and publisher_id column in books table
+        return $this->belongsTo(LocAssignment::class, 'publisher_id');
     }
 
 }
